d6d2e1f56eaee20c8facc7932055e36a70ec8647
RithyaReddy <94788506+RithyaReddy@users.noreply.github.com>
2022-01-15 23:59:52 +0530

    Add files via upload

---
+++ JavaProjects/MiniATM/ClientTest.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class ClientTest {
+    public static void main(String[] args) {
+
+        Account account = new Account();
+        AccountHolder accountHolder = new AccountHolder(account);
+        Thread t1 = new Thread(accountHolder);
+        Thread t2 = new Thread(accountHolder);
+        t1.setName("Jack");
+        t2.setName("Joya");
+
+        t1.start();
+        t2.start();
+    }
+}
